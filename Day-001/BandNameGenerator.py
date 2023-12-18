fe956a37a66437f1fd56e9eb879e6d64edbbb2c4
Youfei Li <64341655+youfeili@users.noreply.github.com>
2023-12-17 23:12:22 -0500

    Create BandNameGenerator.py

---
+++ Day-001/BandNameGenerator.py
@@ -0,0 +1,10 @@
+print("Welcome to the Band name Generator.")
+
+# Ask user for city name
+city = input("What's name of the city you grew up in?\n")
+
+# Ask user for pet name
+pet = input("What's your pet's name?\n")
+
+# Print out the band name
+print("Your band name could be " + city + " " + pet)
