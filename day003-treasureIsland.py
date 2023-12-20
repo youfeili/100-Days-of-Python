bb9025fdc92f197eb4a9b66bee26f2489f5f2065
Youfei Li <64341655+youfeili@users.noreply.github.com>
2023-12-20 11:08:26 -0500

    Update day003-treasureIsland.py

--- day003-treasureIsland.py
+++ day003-treasureIsland.py
@@ -1,11 +1,17 @@
 print("Welcome to Treasure Island.\nYour mission is to find the treasure")
 
-first = input("You're at a cross road. Where do you want to do? Type 'left' or 'right'\n")
+# First prompt the user to start the game
+first = input('You\'re at a cross road. Where do you want to do? Type "left" or "right"\n').lower()
 
+# Check user's input
 if first == 'left':
-    second = input("You come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.\n")
+    # Second prompt for the next stage
+    second = input('You come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n').lower()
+    # Check the user input
     if second == 'wait':
-        third = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose?\n")
+        #Last prompt for the user
+        third = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose?\n").lower()
+        # Check user input
         if third == 'red':
             print("Burned by fire.\nGame Over.")
         elif third == 'yellow':
